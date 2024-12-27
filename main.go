cd7723bf28d920649f884166f71396a126768922
Minou Matoko <mtk14minou@gmail.com>
2024-12-27 22:46:58 +0100

    [WIP]

--- main.go
+++ main.go
@@ -11,13 +11,15 @@ import (
 )
 
 func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
+	// hh := handlers.NewHello(l)
+	// gh := handlers.NewGoodbye(l)
+	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	// sm.Handle("/", hh)
+	// sm.Handle("/goodbye", gh)
+	sm.Handle("/", ph)
 
 	//Server config
 	s := &http.Server{
@@ -27,23 +29,22 @@ func main() {
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
-	//Server Listner
+	//Starting server
 	go func() {
+		l.Println("Starting server on port 9090")
 		err := s.ListenAndServe()
 		if err != nil {
-			l.Fatal(err)
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
 		}
 	}()
 
+	//Shutdown (Gracefully)
 	sigChan := make(chan os.Signal)
 	signal.Notify(sigChan, os.Interrupt)
 	signal.Notify(sigChan, os.Kill)
-
 	sig := <-sigChan
 	l.Println("Received terminate, graceful shutdown", sig)
-
-	//Shutdown (Gracefully)
-
 	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	s.Shutdown(tc)
 }
